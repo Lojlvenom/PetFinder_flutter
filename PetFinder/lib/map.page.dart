daacda05a1f5e2fb2242a8ef2b6f284ae1d3c080
Joseph Levinthal <jvlo@icomp.ufam.edu.br>
2020-08-24 18:04:40 -0400

    Create markers for the dog

--- PetFinder/lib/map.page.dart
+++ PetFinder/lib/map.page.dart
@@ -9,18 +9,55 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   GoogleMapController mapController;
+  Set<Marker> markers = new Set<Marker>();
   double lat = -3.09197916667;
   double lng = -60.0164885;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("PetFinder"),
+        title: TextField(
+          onSubmitted: (val) {
+            lat = -3.09197916667; //Inserir lat FIREBASE
+            lng = -60.0164885; //Inserir lng FIREBASE
+
+            LatLng position = LatLng(lat, lng);
+            mapController.moveCamera(CameraUpdate.newLatLng(position));
+
+            final Marker marker = Marker(
+              markerId:
+                  new MarkerId("1"), //criar ID único com localização dinâmica
+              position: position,
+              infoWindow: InfoWindow(
+                title: "Cachorro 1", //Passar nome de cachorro do FIREBASE
+                snippet: "Hub do Hefesto",
+              ),
+            );
+            setState(() {
+              markers.add(marker);
+            });
+          },
+        ),
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(lat, lng),
-          zoom: 12.0,
+      body: Container(
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          onCameraMove: (data) {
+            print(data);
+          },
+          onTap: (position) {
+            print(position);
+          },
+          initialCameraPosition: CameraPosition(
+            target: LatLng(lat, lng),
+            zoom: 18.0,
+          ),
+          markers: markers,
         ),
       ),
     );
